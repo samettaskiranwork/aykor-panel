122a7ad1b7f8ab752b28fb26fd6b57e44d22924d
samettaskiranwork <samettaskiranwork@gmail.com>
2026-03-04 16:57:15 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"message": "Aykor Panel API running"}
+
+@app.get("/projects")
+def get_projects():
+    return [
+        {"id": 1, "name": "Demo Project"},
+        {"id": 2, "name": "Future Project"}
+    ]
