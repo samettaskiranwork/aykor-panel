fbf356a387a08d1dcb071eaa0143c0ecb915798d
samettaskiranwork <samettaskiranwork@gmail.com>
2026-03-07 18:52:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-from routers import projects # Yeni oluşturduğumuz modül
+from routers import projects
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
-# Modülleri buraya kaydediyoruz
+# Proje modülünü ana sisteme bağlıyoruz
 app.include_router(projects.router)
 
+# Link değişse bile aynı sayfayı yüklemesi için bu yolları tanımladık
 @app.get("/", response_class=HTMLResponse)
 @app.get("/list", response_class=HTMLResponse)
 @app.get("/add", response_class=HTMLResponse)
