5cc5c54cdbc239c794c496e446b212c193c4929e
samettaskiranwork <samettaskiranwork@gmail.com>
2026-03-07 18:46:32 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,1 +1,13 @@
+import os
+import mysql.connector
 
+def get_db_connection():
+    return mysql.connector.connect(
+        host="serverless-eu-central-1.sysp0000.db1.skysql.com",
+        port=4042,
+        user="dbpwf34135244",
+        password=os.getenv("DB_PASSWORD"),
+        database="aykor_dev", 
+        ssl_ca="skysql_ca.pem",
+        ssl_verify_cert=True
+    )
