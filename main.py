1e26f3ebe8772075261fd285c4d6ac0bcbe51ebc
samettaskiranwork <samettaskiranwork@gmail.com>
2026-03-08 13:18:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-from routers import projects
+from routers import projects, budget, future, firms # Hepsini içeri al
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
+# Routerları Kaydet
 app.include_router(projects.router)
+app.include_router(budget.router)
+app.include_router(future.router)
+app.include_router(firms.router)
 
-# F5 YAPILDIĞINDA HATA ALMAMAK İÇİN TÜM YOLLARI BURAYA BAĞLADIK
 @app.get("/", response_class=HTMLResponse)
-@app.get("/list", response_class=HTMLResponse)
-@app.get("/add", response_class=HTMLResponse)
-async def serve_dashboard(request: Request):
+@app.get("/{path}", response_class=HTMLResponse)
+async def serve_dashboard(request: Request, path: Optional[str] = "list"):
     return templates.TemplateResponse("dashboard.html", {"request": request})
