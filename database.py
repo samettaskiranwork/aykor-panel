0788f777f086c759df6e6d9068cbf2c539ec1eab
Samet Taşkıran <samettaskiranwork@gmail.com>
2026-03-19 21:36:16 +0300

    Veritabani baglantisi dinamik ve guvenli hale getirildi

--- database.py
+++ database.py
@@ -1,13 +1,27 @@
-import os
 import mysql.connector
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def get_db_connection():
+    # Render'da mıyız yoksa Samet'in bilgisayarında mı anlamak için basit bir kontrol:
+    # Eğer Render'daysak 'RENDER' diye bir değişken sistemde olur.
+    is_render = os.getenv("RENDER") 
+
+    if is_render:
+        # RENDER AYARI: Dosya yolu sunucuya göre (aykor-panel klasörünün içinde olmalı)
+        ca_path = "globalsignrootca.pem"
+    else:
+        # SENİN BİLGİSAYARIN: Kendi yolun
+        ca_path = r"C:\Users\Samet\Desktop\Kodlar\APY\globalsignrootca.pem"
+
     return mysql.connector.connect(
         host="serverless-eu-central-1.sysp0000.db1.skysql.com",
         port=4042,
         user="dbpwf34135244",
-        password=os.getenv("DB_PASSWORD"),
+        password=os.getenv("DB_PASSWORD"), # Hem yerelde hem Render'da çalışır!
         database="aykor_dev", 
-        ssl_ca="skysql_ca.pem",
+        ssl_ca=ca_path,
         ssl_verify_cert=True
     )
